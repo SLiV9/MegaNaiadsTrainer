9f6f2d882ca109355872446f5c6166d7bcf090c6
Sander in 't Veld <sanderintveld@hotmail.com>
2021-05-28 21:30:40 +0200

    Added CMakeLists.txt and src/main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+#include <chrono>
+#ifdef _MSC_VER
+#include <direct.h>
+#else
+#include <sys/stat.h>
+#endif
+
+static uint64_t currentMilliseconds()
+{
+	auto now = std::chrono::system_clock::now().time_since_epoch();
+	return std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Press Enter to start:" << std::endl;
+	std::cin.ignore();
+
+	srand(currentMilliseconds());
+
+	// TODO something
+}
